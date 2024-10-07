78c1e0a581743f49643e2a844444559b13246da8
Brian Muniz Silveira <155079481+BrianMunizSilveira@users.noreply.github.com>
2024-10-07 16:57:42 -0300

    Pequenas adições e correções:
    - Novos álbuns;
    - Nova imagem de compartilhamento;
    - Arquivos não usados deletados;
    - Botão de retornar ao topo adicionado;
    - Descrições intuitvas para redirecionamento adicionadas;
    - Novas informações no footer da página;

---
+++ discografia-scar/src/js/scarlxrd.js
@@ -0,0 +1,16 @@
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTopButton.classList.add('show');
+    } else {
+        backToTopButton.classList.remove('show');
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
